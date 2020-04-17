a4434b42bd41f55585c0a417c65c062058e7ba78
changxianglin <281532612@qq.com>
2020-04-18 00:33:10 +0800

    finish array

--- Array.js
+++ Array.js
@@ -63,3 +63,155 @@ var d = a.slice(1, 2)
 console.log(b)
 console.log(c)
 console.log(d)
+
+// Array.sort(comparefn)
+var n = [4, 8, 15, 16, 23, 42]
+var m = n.sort()
+console.log(m)
+
+var r = n.sort(function (a, b) {
+    return a - b
+})
+console.log(r)
+
+var m = ['aa', 'bb', 'a', 4, 8, 15, 16, 23, 42]
+var t = m.sort(function (a, b) {
+    if (a === b) {
+        return 0
+    }
+    if (typeof a === typeof b) {
+        return a < b ? -1 : 1
+    }
+    return typeof a < typeof b ? -1 : 1
+})
+console.log(t)
+
+var by = function (name) {
+    return function (o, p) {
+        var a, b;
+        if (typeof o === 'object' && typeof p === 'object' && o && p) {
+            a = o[name]
+            b = p[name]
+            if (a === b) {
+                return 0
+            }
+            if (typeof a === typeof b) {
+                return a < b ? -1 : 1
+            }
+            return typeof a < typeof b ? -1 : 1
+        } else {
+            throw {
+                name: 'Error',
+                message: 'Expected an object when sorting by ' + name
+            }
+        }
+    }
+}
+
+var s = [
+    {first: 'Joe', last: 'Besser'},
+    {first: 'Moe', last: 'Howard'},
+    {first: 'Joe', last: 'DeRita'},
+    {first: 'Shemp', last: 'Howard'},
+    {first: 'Larry', last: 'Fine'},
+    {first: 'Currly', last: 'Howard'},
+]
+
+var ss = s.sort(by('first'))
+console.log(ss)
+
+var xx = s.sort(by('first')).sort(by('last'))
+console.log(xx) // have some question
+
+var by = function (name, minor) {
+    return function (o, p) {
+        var a, b;
+        if (o && p && typeof o === 'object' && typeof p === 'object') {
+            a = o[name]
+            b = p[name]
+            if (a === b) {
+                return typeof minor === "function" ? minor(o, p) : 0
+            }
+            if (typeof a === typeof b) {
+                return a < b ? -1 : 1
+            }
+            return typeof a < typeof b ? -1 : 1
+        } else {
+            throw {
+                name: 'Error',
+                message: 'Expected an object when sotring by ' + name
+            }
+        }
+    }
+}
+
+var ee = s.sort(by('last'), by('first'))
+console.log(ee)
+
+// Array.splice(start, deleteCount, item...)
+var a = ['a', 'b', 'c']
+var r = a.splice(1, 1, 'ache', 'bug')
+console.log(r)
+
+Array.method('splice', function (start, deleteCount) {
+    var max = Math.max,
+        min = Math.min,
+        delta,
+        element,
+        insertCount = max(arguments.length - 2, 0),
+        k = 0,
+        len = this.length,
+        new_len,
+        result = [],
+        shift_count;
+
+    start = start || 0;
+    if (start < 0) {
+        start += len
+    }
+    start = max(min(start, len), 0)
+    deleteCount = max(min(typeof deleteCount === 'number' ? deleteCount : len, len - start), 0)
+    delta = insertCount - deleteCount
+    new_len = len + delta
+    while (k < deleteCount) {
+        element = this[start + k]
+        if (element !== undefined) {
+            result[k] = element
+        }
+        k += 1
+    }
+    shift_count = len - start - deleteCount
+    if (delta < 0) {
+        k = start + insertCount
+        while (shift_count) {
+            this[k] = this[k - delta]
+            k += 1
+            shift_count -= 1
+        }
+        this.length = new_len
+    } else if (delta > 0) {
+        k = 1
+        while (shift_count) {
+            this[new_len - k] = this[len -k]
+            k += 1
+            shift_count -= 1
+        }
+        this.length = new_len
+    }
+    for (k = 0; k < insertCount; k += 1) {
+        this[start + k] = arguments[k + 2]
+    }
+    return result
+})
+
+// Array.unshift(item...)
+var a = ['a', 'b', 'c']
+var r = a.unshift('?', '@')
+console.log(r)
+
+Array.method('unshift', function () {
+    this.splice.apply(
+        this,
+        [0, 0].concat(Array.prototype.splice.apply(arguments)))
+    return this.length
+})
